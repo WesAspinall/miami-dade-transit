4cf99f0d49c95ce42e027c7c644812eac4a238d7
wesaspinall <wes@wesaspinall.com>
2019-12-07 01:10:36 -0500

    added some formatting for when trains are closed

--- src/ArrivalTimes.js
+++ src/ArrivalTimes.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const ArrivalTimes = (props) => {
+
+    const formatter = (data) => {
+        if(!data) {
+            return ' data currently unavailable ';
+        } else if (data === '*****'){
+            return `closed`;
+        } else {
+            return `(arrives in ${data})`;
+        }
+    }
+
     return (
         <div>
             <div>{props.direction}</div>
-            <span>Train 1: {props.station.NB_Time1_Arrival} (arrives in {props.station.NB_Time1}) </span> | <span>Train 2: {props.station.NB_Time2_Arrival} (arrives in {props.station.NB_Time2}) </span> | <span>Train 3: {props.station.NB_Time3_Arrival} (arrives in {props.station.NB_Time3})</span>
+            <span>Train 1: {props.station.NB_Time1_Arrival} {formatter(props.station.NB_Time1)} </span> | 
+            <span>Train 2: {props.station.NB_Time2_Arrival} {formatter(props.station.NB_Time2)}</span>  | 
+            <span>Train 3: {props.station.NB_Time3_Arrival} {formatter(props.station.NB_Time3)}</span>
         </div>
        
     )
