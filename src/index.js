69cd013b0f5f0a0aa13a6bbf23e202af95bdc7a1
wesaspinall <wes@wesaspinall.com>
2019-12-06 22:27:24 -0500

    initial commit, display stations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            stations: []
+        }
+
+    }
+
+    componentDidMount() {
+        fetch('http://miami-transit-api.herokuapp.com/api/TrainStations.json')
+            .then((res) => res.json())
+            .then((data) => this.setState({ stations: data.RecordSet.Record }));
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <h1>Metro Rail Stations</h1>
+                <div className="station-list">
+                    {this.state.stations.map((item, index) => {
+                        return (
+                            <li style={{listStyleType: 'none', paddingLeft: '1em'}}key={index}>{ item.Station }</li>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
